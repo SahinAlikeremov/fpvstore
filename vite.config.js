bd941edb582addaf8e3a92b4da8d34627d0b94f2
Front-End <alikeremovsahin@gmail.com>
2026-08-08 18:35:46 +0400

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
     plugins: [react()],
-    base: import.meta.env.DEV ? "/" : "/fpvstore/",
-});
+    base: command === "serve" ? "/" : "/fpvstore/",
+}));
